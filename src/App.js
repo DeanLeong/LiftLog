0bd6a12f7c0c895569adbc43a802a26fe3e0c1e5
Dean Leong <deanleong@MacBook-Pro.local>
2020-10-09 14:00:05 -0400

    savedworkouts page ready to go

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from "react"
+//import { Router, Switch, Route } from "react-router-dom"
 import './App.css';
-import Workouts from "./Workouts"
+import SavedWorkouts from "./SavedWorkouts"
+import axios from "axios"
 
 function App() {
+
   return (
-    <div className="App">
-      <header>
-        <h1>LiftLog</h1>
-        <Workouts />
-      </header>
+    <div>
+      <div>
+      <h1 className="Header">LiftLog</h1>
+        <nav>
+          <button>Home</button>
+          <button onclick={SavedWorkouts()}>Saved workouts</button>
+          <button>lb to kg calculator</button>
+        </nav>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/SavedWorkouts.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+
+function SavedWorkouts(props) { 
+  const [workouts, setWorkouts] = useState([])
+
+  useEffect(() => {
+    const getWorkouts = async () => {
+      const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/liftlog`
+      const response = await axios.get(airtableURL, {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
+        }
+      })
+      console.log(response.data.records)
+      setWorkouts(response.data.records)
+    }
+    getWorkouts()
+  }, [])
+
+  
+  return (
+    <div>
+    <div>
+        <h1>LiftLog</h1>
+        <nav>
+          Home |
+          Saved workouts |
+          lb to kg calculator |
+        </nav>
+    </div>
+    <div className="savedWorkouts">
+        {workouts.map((workout) => (
+          <p>{workout.fields.Day},
+            {workout.fields.Exercise},
+            {workout.fields.Weight},
+            {workout.fields.Reps},
+            {workout.fields.Notes} </p>
+ 
+        ))}
+    </div>
+  </div>
+  )
+  
+}
+
+export default SavedWorkouts
