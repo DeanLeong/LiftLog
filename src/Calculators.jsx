fb285a7a23d9225716bf2342303ff810a5c97d6c
Dean Leong <deanleong@MacBook-Pro.local>
2020-10-10 11:21:53 -0400

    almost working calculator

--- src/Calculators.jsx
+++ src/Calculators.jsx
@@ -2,13 +2,17 @@ import React, { useState } from "react"
 
 function Calculators() {
   const [value, setValue] = useState("")
+
   function lbsToKgs() {
-    
+    let kgs = value / 2.2046
+    console.log(kgs)
+    return kgs
   }
+  lbsToKgs()
   return (
     <div>
       <h2>Weight Calculators</h2>
-      <form>
+      <form onSubmit={lbsToKgs}>
         <label htmlFor="lbsToKgs">Lbs:</label>
         <input className="lbsInput"
           name="Pounds"
@@ -16,10 +20,15 @@ function Calculators() {
           placeholder="Pounds"
           value={value}
           onChange={(e) => setValue(e.target.value)}
-          />
+        />
+        <button type="submit" className="lbs-to-kgs-submit">Calculate</button>
       </form>
+      {/* <p>{kgs}</p> */}
     </div>
   )
 }
 
 export default Calculators
+
+//I have a functional pounds input
+//I need to take that input and plug it into a mathematical operation
