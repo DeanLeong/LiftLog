7e12749f557cbf4db4a2008767ab9b1b9bfd10a6
Dean Leong <deanleong@MacBook-Pro.local>
2020-10-12 19:00:49 -0400

    footer stays put now

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -4,16 +4,20 @@ function HomePage() {
   return (
     <div>
       <h2>Welcome to Liftlog!</h2>
-      <p>Liftlog is designed to help keep track of your weightlifting workouts.</p>
-      <p className="info-top">Here you can: </p>
-      <div className="homepage-info">
-        <p>Submit a workout.</p>
-        <p>See your saved workouts.</p>
-        <p>Use some weightlifting-related calculators.</p>
+      <div className="homepage-container">
+        <div>
+          <p>Liftlog is designed to help keep track of your weightlifting workouts.</p>
+          <p className="info-top">Here you can: </p>
+        <div className="homepage-info">
+          <p>Submit a workout.</p>
+          <p>See your saved workouts.</p>
+          <p>Use some weightlifting-related calculators.</p>
+        </div>
       </div>
       <div className="feds-embed">
         <embed src="https://www.usapowerlifting.com/"></embed>
         <embed src="https://meets.revolutionpowerlifting.com/"></embed>
+      </div>
       </div>
     </div>
   )
