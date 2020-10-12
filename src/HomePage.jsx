51045ff7595ce9cbdef0d4f3e52ccb9a408469a1
Dean Leong <deanleong@MacBook-Pro.local>
2020-10-12 18:05:03 -0400

    some homepage ideas

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -7,11 +7,13 @@ function HomePage() {
       <p>Liftlog is designed to help keep track of your weightlifting workouts.</p>
       <p className="info-top">Here you can: </p>
       <div className="homepage-info">
-      <ol className="homepage-list">
-        <li>Submit a workout.</li>
-        <li>See your saved workouts.</li>
-        <li>Use some weightlifting-related calculators.</li>
-      </ol>
+        <p>Submit a workout.</p>
+        <p>See your saved workouts.</p>
+        <p>Use some weightlifting-related calculators.</p>
+      </div>
+      <div className="feds-embed">
+        <embed src="https://www.usapowerlifting.com/"></embed>
+        <embed src="https://meets.revolutionpowerlifting.com/"></embed>
       </div>
     </div>
   )
