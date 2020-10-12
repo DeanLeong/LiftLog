84b660f97369f3fbb59e6bb39c9f977ec4e0292e
Dean Leong <deanleong@MacBook-Pro.local>
2020-10-12 12:13:30 -0400

    footer work

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,11 @@ import React from "react"
 //Link my socials/contact info
 function Footer() {
   return (
-    <p className="footer">Dean Leong 2020</p>
+    <div className="footer">
+      <p className="footer">Dean Leong 2020</p>
+      <a href="https://github.com/DeanLeong">Github</a>
+      <a href="https://www.linkedin.com/in/dean-leong-2a68041b2/">LinkedIn</a>
+    </div>
   )
 }
 
