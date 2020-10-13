5031fe2ab1e85b8e995be01512d04232945fdf51
Dean Leong <deanleong@MacBook-Pro.local>
2020-10-13 16:43:19 -0400

    homepage styling

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,7 +5,7 @@ function HomePage() {
     <div>
       <h2>Welcome to Liftlog!</h2>
       <div className="homepage-container">
-        <div>
+        <div className="info-box">
           <p>Liftlog is designed to help keep track of your weightlifting workouts.</p>
           <p className="info-top">Here you can: </p>
         <div className="homepage-info">
